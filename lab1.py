33d4b96dcdbd860d6bd1e74d69c1dbecab5290d8
John Doe <vandon0013@gmail.com>
2025-01-15 12:39:33 +0500

    Python Syntax

--- lab1.py
+++ lab1.py
@@ -4,3 +4,15 @@ import sys
 # Intro
 print("Hello, World!")
 print(sys.version)
+
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+    print("Five is greater than two!")
+    print("Five is greater than two!")
+x = 5
+y = "Hello World!"
+#This is a comment.
+print(y)
