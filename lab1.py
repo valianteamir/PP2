a94118d5d506c36f4cd4200a80cad2698787288d
John Doe <vandon0013@gmail.com>
2025-01-18 14:42:06 +0500

    python strings 1

--- lab1.py
+++ lab1.py
@@ -205,4 +205,38 @@ z = float("3")   # z will be 3.0
 w = float("4.2") # w will be 4.2
 x = str("s1") # x will be 's1'
 y = str(2)    # y will be '2'
-z = str(3.0)  # z will be '3.0'
+z = str(3.0)  # z will be '3.0
+# python strings
+print("Hello")
+print('Hello')
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+a = "Hello"
+print(a)
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+a = "Hello, World!"
+print(a[1])
+for x in "banana":
+  print(x)
+a = "Hello, World!"
+print(len(a))
+txt = "The best things in life are free!"
+print("free" in txt)
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
