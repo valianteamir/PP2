fcef82d398c585cf6ff2133c29028d57d097e81f
John Doe <vandon0013@gmail.com>
2025-01-19 21:41:47 +0500

    Python Strings

--- lab1.py
+++ lab1.py
@@ -240,3 +240,43 @@ print("expensive" not in txt)
 txt = "The best things in life are free!"
 if "expensive" not in txt:
   print("No, 'expensive' is NOT present.")
+  b = "Hello, World!"
+print(b[2:5])
+b = "Hello, World!"
+print(b[:5])
+b = "Hello, World!"
+print(b[2:])
+b = "Hello, World!"
+print(b[-1:-10])
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+a = "Hello, World!"
+print(a.replace("H", "J"))
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
