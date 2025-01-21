4b8e64694da51930ba7fd4268e93dd84526b0ea2
John Doe <vandon0013@gmail.com>
2025-01-21 20:59:07 +0500

    Python Lists + Python If Else

--- lab2.py
+++ lab2.py
@@ -49,3 +49,232 @@ print(10 + 5)
 print((6 + 3) - (6 + 3))
 print(100 + 5 * 3)
 print(5 + 4 - 7 + 3)
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+list1 = ["abc", 34, True, 40, "male"]
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5]) 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+del thislist
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+  thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i += 1
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+newlist = [x for x in fruits if x != "apple"]
+newlist = [x for x in fruits]
+newlist = [x for x in range(10)]
+print(newlist)
+newlist = [x for x in range(10) if x < 5]
+newlist = [x.upper() for x in fruits]
+newlist = ['hello' for x in fruits]
+print(newlist)
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+if a > b: print("a is greater than b")
+a = 2
+b = 330
+print("A") if a > b else print("B")
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+a = 33
+b = 200
+
+if b > a:
+  pass        
