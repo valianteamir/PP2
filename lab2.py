2543d76cd4fb3e076e810623120097e74002a7f0
John Doe <vandon0013@gmail.com>
2025-01-22 12:38:37 +0500

    Python While Loops + Python Tuples

--- lab2.py
+++ lab2.py
@@ -278,3 +278,121 @@ b = 200
 
 if b > a:
   pass        
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+tuple1 = ("abc", 34, True, 40, "male")
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)  
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+print(thistuple)
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+# print(thistuple) #this will raise an error because the tuple no longer exists
+fruits = ("apple", "banana", "cherry")
+fruits = ("apple", "banana", "cherry")
+
+green, yellow, red = fruits
+
+print(green)
+print(yellow)
+print(red)
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+print(green)
+print(yellow)
+print(red)
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")        
