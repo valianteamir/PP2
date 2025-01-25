74ef37a0f7a7275464b24475b32eb021e4b5be95
John Doe <vandon0013@gmail.com>
2025-01-25 19:30:09 +0500

    Python For Loops + Python Sets

--- lab2.py
+++ lab2.py
@@ -396,3 +396,216 @@ while i < 6:
   i += 1
 else:
   print("i is no longer less than 6")        
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+set1 = {"abc", 34, True, 40, "male"}
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" not in thisset)
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+del thisset
+
+# print(thisset)  
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1 | set2
+print(set3)
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1.union(set2, set3, set4)
+print(myset)
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1 | set2 | set3 |set4
+print(myset)
+x = {"a", "b", "c"}
+y = (1, 2, 3)
+
+z = x.union(y)
+print(z)
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.intersection(set2)
+print(set3)
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 & set2
+print(set3)
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.intersection_update(set2)
+
+print(set1)
+set1 = {"apple", 1,  "banana", 0, "cherry"}
+set2 = {False, "google", 1, "apple", 2, True}
+
+set3 = set1.intersection(set2)
+
+print(set3)
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.difference(set2)
+
+print(set3)
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 - set2
+print(set3)
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+print(set1)
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.symmetric_difference(set2)
+
+print(set3) 
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 ^ set2
+print(set3)
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.symmetric_difference_update(set2)
+
+print(set1)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+for x in "banana":
+  print(x)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+for x in range(2, 30, 3):
+  print(x)
+for x in range(2, 6):
+  print(x)
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+for x in [0, 1, 2]:
+  pass
